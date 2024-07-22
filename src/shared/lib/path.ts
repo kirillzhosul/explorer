467939933fa0a2bcd220028d112eda5c738485a7
Stepan Zubkov <zubkovbackend@gmail.com>
2024-07-22 19:30:47 +0300

    feat: Remove all trailing slashes when trying to get target path.

--- src/shared/lib/path.ts
+++ src/shared/lib/path.ts
@@ -54,9 +54,7 @@ export function getPathTarget(path: string, os_path_style: OsPathStyle = OS_DEFA
   */
   if (os_path_style == OsPathStyle.posix) {
     if (path == OsPathSeparator.posix) return path
-    if (path.endsWith(OsPathSeparator.posix)) {
-      path = path.slice(0, path.length - 1)
-    }
+    path = path.replace(/\/+$/g, "");
   }
 
   const segments = splitPath(path, os_path_style)
