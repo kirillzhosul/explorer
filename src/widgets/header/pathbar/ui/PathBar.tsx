2a21ce1cd03ada74e17499962d21bf4419cbf6f9
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2024-07-26 02:29:12 +0400

    fix: 🩹 Fix key-render warning for pathbar

--- src/widgets/header/pathbar/ui/PathBar.tsx
+++ src/widgets/header/pathbar/ui/PathBar.tsx
@@ -64,7 +64,7 @@ export function PathBar({ fullPath, onChangePath }: PATH_BAR_PROPS) {
           </div>
           {displaySegments.map((pathSegment, pathIndex) => {
             return (
-              <div className={styles.pathBarCompositeSegment} key={pathSegment}>
+              <div className={styles.pathBarCompositeSegment} key={pathIndex + pathSegment}>
                 <div
                   className={styles.pathBarSegment}
                   onClick={() => {
