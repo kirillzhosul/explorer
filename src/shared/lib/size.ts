1375e9e1f82af98a7b3ef300cc3a5ecac50792bd
Emil <40684650+MrCheatEugene@users.noreply.github.com>
2024-07-26 03:23:29 +0300

    Fix

--- src/shared/lib/size.ts
+++ src/shared/lib/size.ts
@@ -1,10 +1,11 @@
-export function getReadableFileSizeString(byteSize: number) {
+export function getReadableFileSizeString(byteSize: number, asSI:boolean=false) {
   let i = -1;
+  let unit = asSI ? 1000 : 1024;
   let byteUnits = [" kB", " MB", " GB", " TB", "PB", "EB", "ZB", "YB"];
   do {
-    byteSize /= 1024;
+    byteSize /= unit;
     i++;
-  } while (byteSize > 1024);
+  } while (byteSize > unit);
 
   return Math.max(byteSize, 0.1).toFixed(1) + byteUnits[i];
 }
