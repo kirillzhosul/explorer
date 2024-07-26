5928781761af7668304361476eaf83b1911b1c7d
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2024-07-26 05:26:47 +0400

    fix: 🩹 Fix file size SI mapping names, add tests for SI sizes

--- src/shared/lib/size.ts
+++ src/shared/lib/size.ts
@@ -1,11 +1,18 @@
-export function getReadableFileSizeString(byteSize: number, asSI:boolean=false) {
+export function getReadableFileSizeString(byteSize: number, asSI: boolean = false) {
   let i = -1;
-  let unit = asSI ? 1000 : 1024;
-  let byteUnits = [" kB", " MB", " GB", " TB", "PB", "EB", "ZB", "YB"];
+  let unit = 1024
+  let mapping = ["KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+
+  if (asSI) {
+    unit = 1000;
+    mapping = ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+  }
+
   do {
     byteSize /= unit;
     i++;
   } while (byteSize > unit);
 
-  return Math.max(byteSize, 0.1).toFixed(1) + byteUnits[i];
+  let size = Math.max(byteSize, 0.1).toFixed(1)
+  return `${size} ${mapping[i]}`;
 }
--- src/tests/size.test.ts
+++ src/tests/size.test.ts
@@ -2,9 +2,17 @@ import { expect, test } from "vitest";
 import { getReadableFileSizeString } from "../shared/lib/size";
 
 test("getReadableFileSizeString", () => {
-    expect(getReadableFileSizeString(0)).toEqual("0.1 kB");
-    expect(getReadableFileSizeString(15)).toEqual("0.1 kB");
-    expect(getReadableFileSizeString(1024)).toEqual("1.0 kB");
-    expect(getReadableFileSizeString(2400)).toEqual("2.3 kB");
-    expect(getReadableFileSizeString(9149999)).toEqual("8.7 MB");
+    expect(getReadableFileSizeString(0, false)).toEqual("0.1 KB");
+    expect(getReadableFileSizeString(15, false)).toEqual("0.1 KB");
+    expect(getReadableFileSizeString(1024, false)).toEqual("1.0 KB");
+    expect(getReadableFileSizeString(2400, false)).toEqual("2.3 KB");
+    expect(getReadableFileSizeString(9149999, false)).toEqual("8.7 MB");
+});
+
+test("getReadableFileSizeStringSI", () => {
+    expect(getReadableFileSizeString(0, true)).toEqual("0.1 KiB");
+    expect(getReadableFileSizeString(15, true)).toEqual("0.1 KiB");
+    expect(getReadableFileSizeString(1024, true)).toEqual("1.0 KiB");
+    expect(getReadableFileSizeString(2400, true)).toEqual("2.4 KiB");
+    expect(getReadableFileSizeString(9149999, true)).toEqual("9.1 MiB");
 });
